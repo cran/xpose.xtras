36b619e6d541273bbd0ea12c424d0e91aad824ae
John Prybylski <jprybylski@gmail.com>
2025-09-09 06:50:02 +0000

    version 0.1.0

--- inst/doc/a03-useful_plots.R
+++ inst/doc/a03-useful_plots.R
@@ -15,13 +15,14 @@ pheno_set %>%
   ipred_vs_idv_modavg(auto_backfill = TRUE, quiet=TRUE)
 
 ## ----m3, fig.width=unit(7,"in"), fig.height=unit(5,"in")----------------------
-pkpd_m3 %>%
+described_pkpd_m3 <- pkpd_m3 %>%
   # Need to ensure var types are set
   set_var_types(catdv=BLQ,dvprobs=LIKE) %>%
   # Set probs ("LIKE is the probability tht BLQ is 1")
   set_dv_probs(1, 1~LIKE, .dv_var = BLQ) %>%
   # Optional, but useful to set levels
-  set_var_levels(1, BLQ = lvl_bin()) %>%
+  set_var_levels(1, BLQ = lvl_bin())
+described_pkpd_m3 %>%
   catdv_vs_dvprobs(quiet=TRUE)
 
 ## ----vismo, fig.width=unit(7,"in"), fig.height=unit(5,"in")-------------------
@@ -34,6 +35,17 @@ vismo_xpdb %>%
   catdv_vs_dvprobs(cutpoint = 2, quiet=TRUE)
 vismo_xpdb %>%
   catdv_vs_dvprobs(cutpoint = 3, quiet=TRUE)
+
+## ----roc_ex1, fig.width=unit(7,"in"), fig.height=unit(5,"in")-----------------
+described_pkpd_m3 %>%
+  roc_plot(quiet=TRUE)
+vismo_xpdb %>%
+  # Epsilon shrinkage is still included in default subtitle for M3-like use cases
+  roc_plot(cutpoint = 2, quiet=TRUE, subtitle = "Ofv: @ofv") 
+
+## ----roc_ex2, fig.width=unit(7,"in"), fig.height=unit(5,"in")-----------------
+described_pkpd_m3 %>%
+  roc_plot(quiet=TRUE, group="ID", type="pt")
 
 ## ----waterfall, fig.width=unit(7,"in"), fig.height=unit(5,"in")---------------
 pheno_set %>%
